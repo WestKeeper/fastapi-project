5be3d6897e85240c7ef957783feb6d5a5ef9e4c1
Anton Semenyuta <antonsemenyuta@yandex.ru>
2022-10-24 15:00:27 +0700

    Added schemas, db models and dependency injection

--- backend/main.py
+++ backend/main.py
@@ -5,25 +5,27 @@ from fastapi.staticfiles import StaticFiles
 from apis.general_pages.route_homepage import general_pages_router
 from core.config import settings
 from db.session import engine
-from db.base_class import Base
+from db.base import Base
 
 
 def include_router(app):
 	app.include_router(general_pages_router)
 
 
+def create_tables():
+	print("create_tables")
+	Base.metadata.create_all(bind=engine)
+
+
 def configure_static(app):
 	app.mount('/static', StaticFiles(directory='static'), name='static')
-
-
-def create_tables():           #new
-	Base.metadata.create_all(bind=engine)
 
 
 def start_application():
 	app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
 	include_router(app)
 	configure_static(app)
+	create_tables()
 	return app
 
 
